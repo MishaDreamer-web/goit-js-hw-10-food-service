93160974e52a546237119309478d9b7fd578d0e6
MishaDreamer-web <shondayn@gmail.com>
2021-05-17 22:59:09 +0300

    Уменшает код и убирает лишнюю функцию в index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import menuTemplate from "./templates/templates.hbs";
-import menuItems from "./menu.json";
+import menuTemplate from './templates/templates.hbs';
+import menuItems from './menu.json';
 
-import "./styles.css";
-import "./theme-changer.js";
+import './styles.css';
+import './theme-changer.js';
 
-const foodMenuContainer = document.querySelector(".js-menu");
-const menuMarkup = makeMenuPage(menuItems);
+const foodMenuContainer = document.querySelector('.js-menu');
+const menuMarkup = menuTemplate(menuItems);
+// const menuMarkup = makeMenuPage(menuItems);
 
-foodMenuContainer.insertAdjacentHTML("beforeend", menuMarkup);
+foodMenuContainer.insertAdjacentHTML('beforeend', menuMarkup);
 
-function makeMenuPage(menuItems) {
-  // return menuItems.map(menuTemplate).join("");
-  return menuTemplate(menuItems);
-}
+// function makeMenuPage(menuItems) {
+//   // return menuItems.map(menuTemplate).join("");
+//   return menuTemplate(menuItems);
+// }
 
 // Добавляет все на основе своей зборки парселя
